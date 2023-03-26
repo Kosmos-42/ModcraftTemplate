4db64194a4baf6e47a4779eef99729bb6264e563
Kosmos-42 <come.nrth@gmail.com>
2023-03-26 20:17:41 +0200

    Adding Three new blocks with their associated textures/items.

--- src/main/java/com/Kosmos42/modcraft_template/ModcraftTemplate.java
+++ src/main/java/com/Kosmos42/modcraft_template/ModcraftTemplate.java
@@ -1,6 +1,7 @@
 package com.Kosmos42.modcraft_template;
 
 import com.Kosmos42.modcraft_template.Item.ModItems;
+import com.Kosmos42.modcraft_template.block.ModBlocks;
 import com.mojang.logging.LogUtils;
 import net.minecraftforge.common.MinecraftForge;
 import net.minecraftforge.eventbus.api.IEventBus;
@@ -21,6 +22,7 @@ public class ModcraftTemplate {
         IEventBus modEventBus = FMLJavaModLoadingContext.get().getModEventBus();
 
         ModItems.register(modEventBus);
+        ModBlocks.register(modEventBus);
 
         modEventBus.addListener(this::commonSetup);
 
