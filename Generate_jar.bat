dd0fe9858b266409a47344c2a1b3ace3b9266dd0
Kosmos-42 <come.nrth@gmail.com>
2023-04-20 20:13:16 +0200

    Version finale du projet

--- Generate_jar.bat
+++ Generate_jar.bat
@@ -1,4 +1,10 @@
 rmdir src\main\resources\assets /Q /S
 xcopy resources\ src\main\resources /E /I /H /Y
+@echo off
+echo -----------------------------------------------------------------
+echo Les resources sont bien chargees.
+echo Le jar est pret a etre genere
+echo -----------------------------------------------------------------
+pause
 .\gradlew jar
 pause
